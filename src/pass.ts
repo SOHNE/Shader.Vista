f82df95afa0abfa66f7615dd472aafdb5ed0b113
Leandro Peres <leandroperes@protonmail.com>
2024-06-25 13:10:27 -0300

    Refactoration + draw

--- src/pass.ts
+++ src/pass.ts
@@ -1,4 +1,4 @@
-import Shader from "./shader";
+import Shader from './shader';
 
 export default class Pass {
   gl: WebGLRenderingContext;
@@ -10,6 +10,8 @@ export default class Pass {
   next: Pass | null;
   offscreen: boolean;
   textures: WebGLTexture[];
+  positionBuffer: WebGLBuffer;
+  positionAttributeLocation: number;
 
   constructor(
     gl: WebGLRenderingContext,
@@ -23,15 +25,19 @@ export default class Pass {
     this.shader = shader;
     this.width = width;
     this.height = height;
-    this.texture = <WebGLTexture>this.createTexture();
-    this.framebuffer = <WebGLFramebuffer>this.createFramebuffer(this.texture);
+    this.texture = this.createTexture() as WebGLTexture;
+    this.framebuffer = this.createFramebuffer(this.texture) as WebGLFramebuffer;
     this.next = null;
     this.offscreen = offscreen;
     this.textures = textures;
+    this.positionAttributeLocation =
+      this.shader.getAttribLocation('a_position');
+    this.positionBuffer = this.createPositionBuffer() as WebGLBuffer;
   }
 
   use() {
     this.shader.use();
+
     if (this.offscreen) {
       this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.framebuffer);
       this.gl.viewport(0, 0, this.width, this.height);
@@ -39,7 +45,65 @@ export default class Pass {
       this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
       this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
     }
-    this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+  }
+
+  draw() {
+    this.use();
+
+    this.textures.forEach((texture, index) => {
+      this.gl.activeTexture(this.gl.TEXTURE0 + index);
+      this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+      this.shader.setUniform(`u_texture${index}`, '1i', index);
+    });
+
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.positionBuffer);
+    this.gl.enableVertexAttribArray(this.positionAttributeLocation);
+    this.gl.vertexAttribPointer(
+      this.positionAttributeLocation,
+      2,
+      this.gl.FLOAT,
+      false,
+      0,
+      0
+    );
+
+    this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
+  }
+
+  resize(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+
+    this.gl.bindTexture(this.gl.TEXTURE_2D, this.texture);
+    this.gl.texImage2D(
+      this.gl.TEXTURE_2D,
+      0,
+      this.gl.RGBA,
+      this.width,
+      this.height,
+      0,
+      this.gl.RGBA,
+      this.gl.UNSIGNED_BYTE,
+      null
+    );
+
+    this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.framebuffer);
+    this.gl.framebufferTexture2D(
+      this.gl.FRAMEBUFFER,
+      this.gl.COLOR_ATTACHMENT0,
+      this.gl.TEXTURE_2D,
+      this.texture,
+      0
+    );
+
+    const status = this.gl.checkFramebufferStatus(this.gl.FRAMEBUFFER);
+    if (status !== this.gl.FRAMEBUFFER_COMPLETE) {
+      throw new Error(`Framebuffer not complete: ${status.toString()}`);
+    }
+
+    this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
   }
 
   createTexture() {
@@ -95,5 +159,16 @@ export default class Pass {
     }
     this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
     return framebuffer;
+  }
+
+  createPositionBuffer() {
+    const positionBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, positionBuffer);
+    this.gl.bufferData(
+      this.gl.ARRAY_BUFFER,
+      new Float32Array([-1, -1, 1, -1, -1, 1, -1, 1, 1, -1, 1, 1]),
+      this.gl.STATIC_DRAW
+    );
+    return positionBuffer;
   }
 }
