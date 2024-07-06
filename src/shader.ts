e00da44e1a2596678e49bb50d8f5bf3bde9f35ba
Leandro Peres <leandroperes@protonmail.com>
2024-07-06 11:24:30 -0300

    :beers: onError implementation

--- src/shader.ts
+++ src/shader.ts
@@ -1,103 +1,129 @@
 export default class Shader {
-  gl: WebGLRenderingContext;
-  program: WebGLProgram;
+  private gl: WebGLRenderingContext;
+  private program: WebGLProgram;
+  private uniformLocations: { [name: string]: WebGLUniformLocation } = {};
+  private onError: (details: {
+    passName: string;
+    coords: { line: number; message: string };
+  }) => void;
+  private passName: string;
 
   constructor(
     gl: WebGLRenderingContext,
     vertexSource: string | undefined,
-    fragmentSource: string
+    fragmentSource: string,
+    onError: (details: {
+      passName: string;
+      coords: { line: number; message: string };
+    }) => void,
+    passName: string
   ) {
     this.gl = gl;
+    this.onError = onError;
+    this.passName = passName;
 
-    // Ensure default vertex shader is used if not provided
-    vertexSource = vertexSource || this.defaultVertexShader();
-
-    //console.log('Vertex Shader Source:', vertexSource);
-    //console.log('Fragment Shader Source:', fragmentSource);
-
-    const vertexShader = this.compileShader(
-      this.gl.VERTEX_SHADER,
-      vertexSource
+    let vertexShader: WebGLShader | undefined;
+    vertexShader = this.compileShader(
+      vertexSource || this.defaultVertexShader(),
+      gl.VERTEX_SHADER
     );
+
     const fragmentShader = this.compileShader(
-      this.gl.FRAGMENT_SHADER,
-      fragmentSource
+      fragmentSource,
+      gl.FRAGMENT_SHADER
     );
-
     this.program = this.linkProgram(vertexShader, fragmentShader);
   }
 
-  private compileShader(type: number, source: string): WebGLShader {
-    const shader = this.gl.createShader(type) as WebGLShader;
+  private compileShader(source: string, type: number): WebGLShader {
+    const shader = this.gl.createShader(type);
+    if (!shader) throw new Error('Failed to create shader');
+
     this.gl.shaderSource(shader, source);
     this.gl.compileShader(shader);
 
     if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
-      const info = this.gl.getShaderInfoLog(shader);
-      console.error(`Could not compile shader: ${info}`);
-      console.error(`Shader source:\n${source}`);
+      const log = this.gl.getShaderInfoLog(shader);
       this.gl.deleteShader(shader);
-      throw new Error(`Could not compile shader: ${info}`);
+      const coords = this.extractErrorCoords(log || '');
+      this.onError({
+        passName: this.passName,
+        coords,
+      });
+      throw new Error(`Failed to compile shader: ${log}`);
     }
+
     return shader;
   }
 
+  private extractErrorCoords(log: string): {
+    line: number;
+    message: string;
+  } {
+    const match = /ERROR: 0:(\d+): (.*?)(?=\n|$)/.exec(log);
+    if (match) {
+      return {
+        line: parseInt(match[1], 10),
+        message: match[2],
+      };
+    }
+    return { line: 0, message: '' };
+  }
+
   private linkProgram(
-    vertexShader: WebGLShader,
+    vertexShader: WebGLShader | undefined,
     fragmentShader: WebGLShader
   ): WebGLProgram {
-    const program = this.gl.createProgram() as WebGLProgram;
-    this.gl.attachShader(program, vertexShader);
+    const program = this.gl.createProgram();
+    if (!program) throw new Error('Failed to create program');
+
+    if (vertexShader) this.gl.attachShader(program, vertexShader);
     this.gl.attachShader(program, fragmentShader);
     this.gl.linkProgram(program);
 
     if (!this.gl.getProgramParameter(program, this.gl.LINK_STATUS)) {
-      const info = this.gl.getProgramInfoLog(program);
-      console.error(`Could not link program: ${info}`);
+      const log = this.gl.getProgramInfoLog(program);
       this.gl.deleteProgram(program);
-      throw new Error(`Could not link program: ${info}`);
+      throw new Error(`Failed to link program: ${log}`);
     }
+
     return program;
   }
 
-  use() {
+  public use(): void {
     this.gl.useProgram(this.program);
   }
 
-  getAttribLocation(name: string): number {
-    return this.gl.getAttribLocation(this.program, name);
-  }
-
-  getUniformLocation(name: string): WebGLUniformLocation | null {
-    return this.gl.getUniformLocation(this.program, name);
-  }
-
-  setUniform(name: string, type: string, value: any) {
-    const location = this.getUniformLocation(name);
-    if (location === null) {
-      //console.warn(`Uniform '${name}' not found in shader program.`);
-      return;
+  public setUniform(name: string, type: string, ...values: any[]): void {
+    if (!this.uniformLocations[name]) {
+      this.uniformLocations[name] = this.gl.getUniformLocation(
+        this.program,
+        name
+      ) as WebGLUniformLocation;
     }
 
+    const location = this.uniformLocations[name];
     switch (type) {
-      case '1i':
-        this.gl.uniform1i(location, value);
-        break;
       case '1f':
-        this.gl.uniform1f(location, value);
+        this.gl.uniform1f(location, values[0]);
+        break;
+      case '1i':
+        this.gl.uniform1i(location, values[0]);
         break;
       case '2fv':
-        this.gl.uniform2fv(location, value);
+        this.gl.uniform2fv(location, values[0]);
         break;
       case '3fv':
-        this.gl.uniform3fv(location, value);
+        this.gl.uniform3fv(location, values[0]);
         break;
       case '4fv':
-        this.gl.uniform4fv(location, value);
+        this.gl.uniform4fv(location, values[0]);
         break;
-      default:
-        throw new Error(`Unknown uniform type: ${type}`);
     }
+  }
+
+  public getAttribLocation(name: string): number {
+    return this.gl.getAttribLocation(this.program, name);
   }
 
   private defaultVertexShader(): string {
@@ -105,7 +131,7 @@ export default class Shader {
       attribute vec4 a_position;
       void main() {
         gl_Position = a_position;
-      }
-    `;
+      }`;
   }
 }
+
